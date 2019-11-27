0d280afa13c0ea788af5d249c6703fd0b3f14661
Panagiotis Makris <panosmakris7@windowslive.com>
2019-11-27 17:58:23 +0200

    Main menu constructed

--- src/com/slg/Main.java
+++ src/com/slg/Main.java
@@ -7,14 +7,56 @@ import java.util.Arrays;
 public class Main {
 
     public static final String COMMA_DELIMITER = ";";
-    static ArrayList<Person> allPersons;
+    public static ArrayList<Person> allPersons;
 
     public static void main(String[] args) {
-        // write your code here
+
+
+        MainMenu menu = new MainMenu();
+        menu.showMenu();
+
+        System.out.println("All persons are: " + allPersons.size());
+
+
+        //Father/son
+//        relationFinder("Grandpa Duck","Quackmore Duck");
+//        relationFinder("Quackmore Duck","Grandpa Duck");
+
+        //Siblings
+//        relationFinder("1 (Duck) Von Drake","Quackmore Duck");
+//        relationFinder("Quackmore Duck","1 (Duck) Von Drake");
+
+        //Ksaderfia -- NOT WORKING --> WORKING NOW
+//        relationFinder("May","Huey Duck");
+//        relationFinder("Huey Duck","May");
+
+        //Husband
+//        relationFinder("Quackmore Duck","Hortense (McDuck) Duck");
+//        relationFinder("Hortense (McDuck) Duck","Quackmore Duck");
+
+        //Grandpa
+//        relationFinder("Quackmore Duck","Louie Duck");
+//        relationFinder("Grandma Duck","Gus Goose");
+
+        //Eggonos -- NOT WORKING -- > ALSO WORKING NOW
+//        relationFinder("Louie Duck","Quackmore Duck");
+//        relationFinder("Gus Goose","Grandma Duck");
+
+        //Anipsi
+//        relationFinder("Louie Duck","Daisy Duck");
+//        relationFinder("Gus Goose","Quackmore Duck");
+
+        //Uncle
+//        relationFinder("Daisy Duck","Louie Duck");
+//        relationFinder("Quackmore Duck","Gus Goose");
+
+        //Not related -- NOT WORKING
+//        relationFinder("Daisy Duck","Quackmore Duck");
+
+    }
+
+    public static void readFamilyTree() {
         File file = new File("C:\\Users\\pmakris\\Desktop\\ergasia.csv");
-        allPersons = new ArrayList<>();
-
-
         try {
             BufferedReader csvReader = new BufferedReader(new FileReader(file));
             String row;
@@ -57,209 +99,179 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("All persons are: " + allPersons.size());
-
-
-        //Father/son
-//        relationFinder("Grandpa Duck","Quackmore Duck");
-//        relationFinder("Quackmore Duck","Grandpa Duck");
-
-        //Siblings
-//        relationFinder("1 (Duck) Von Drake","Quackmore Duck");
-//        relationFinder("Quackmore Duck","1 (Duck) Von Drake");
-
-        //Ksaderfia -- NOT WORKING --> WORKING NOW
-//        relationFinder("May","Huey Duck");
-//        relationFinder("Huey Duck","May");
-
-        //Husband
-//        relationFinder("Quackmore Duck","Hortense (McDuck) Duck");
-//        relationFinder("Hortense (McDuck) Duck","Quackmore Duck");
-
-        //Grandpa
-//        relationFinder("Quackmore Duck","Louie Duck");
-//        relationFinder("Grandma Duck","Gus Goose");
-
-        //Eggonos -- NOT WORKING -- > ALSO WORKING NOW
-        relationFinder("Louie Duck","Quackmore Duck");
-        relationFinder("Gus Goose","Grandma Duck");
-
-        //Anipsi
-//        relationFinder("Louie Duck","Daisy Duck");
-//        relationFinder("Gus Goose","Quackmore Duck");
-
-        //Uncle
-//        relationFinder("Daisy Duck","Louie Duck");
-//        relationFinder("Quackmore Duck","Gus Goose");
-
-        //Not related
-        relationFinder("Daisy Duck","Quackmore Duck");
-
     }
 
-    private static void relationFinder(String name1, String name2) {
+    public static void relationFinder(String name1, String name2) {
         Person p1 = getPersonByName(name1);
         Person p2 = getPersonByName(name2);
 
-        //Father
-        if (p1.getFather() != null){
-            if (p1.getFather().name.equals(p2.name)) {
-                if (p1.sex.equals("male")) {
-                    System.out.println(p1.name + " is son of " + p2.name);
-                } else {
-                    System.out.println(p1.name + " is daughter of " + p2.name);
-                }
-            }
-        }
 
-        //Mother
-        if (p1.getMother() != null) {
-            if (p1.getMother().name.equals(p2.name)) {
-                if (p1.sex.equals("male")) {
-                    System.out.println(p1.name + " is son of " + p2.name);
-                } else {
-                    System.out.println(p1.name + " is daughter of " + p2.name);
-                }
-            }
-        }
-
-        //Children
-        for (Person child : p1.children) {
-            if (child.name.equals(p2.name)) {
-                if (p1.sex.equals("male")) {
-                    System.out.println(p1.name + " is father of " + p2.name);
-                } else {
-                    System.out.println(p1.name + " is mother of " + p2.name);
-                }
-            }
-        }
-
-        //Married
-        for (Person person : p1.married) {
-            if (person.name.equals(p2.name)) {
-                System.out.println(p1.name + " is married with " + p2.name);
-            }
-        }
-
-        ///Siblings
-        for (Person sibling : p1.siblings) {
-            if (sibling.name.equals(p2.name)) {
-                if (p1.sex.equals("male")) {
-                    System.out.println(p1.name + " is brother of " + p2.name);
-                } else {
-                    System.out.println(p1.name + " is sister of " + p2.name);
-                }
-            }
-        }
-
-        //Cousins
-        if (p1.getFather() != null) {
-            for (Person sibling : p1.getFather().siblings) {
-                for (Person child : sibling.children) {
-                   if (child.name.equals(p2.name)) {
-                       System.out.println(p1.name + " is cousin with " + p2.name + " from " + p1.getFather().name + " and " + sibling.name);
-                   }
-                }
-            }
-        }
-
-        if (p1.getMother() != null) {
-            for (Person sibling : p1.getMother().siblings) {
-                for (Person child : sibling.children) {
-                    if (child.name.equals(p2.name)) {
-                        System.out.println(p1.name + " is cousin with " + p2.name + " from " + p1.getMother().name + " and " + sibling.name);
+        if (p1 != null && p2 != null) {
+            //Father
+            if (p1.getFather() != null) {
+                if (p1.getFather().name.equals(p2.name)) {
+                    if (p1.sex.equals("male")) {
+                        System.out.println(p1.name + " is son of " + p2.name);
+                    } else {
+                        System.out.println(p1.name + " is daughter of " + p2.name);
                     }
                 }
             }
-        }
 
-
-
-        //Eggonia
-        if (p1.getFather() != null) {
-            if (p1.getFather().getFather() != null) {
-                if (p1.getFather().getFather().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos toy " + p2.name);
-                }
-                if (p1.getFather().getMother().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos ths " + p2.name);
-                }
-            } else if (p1.getFather().getMother() != null) {
-                if (p1.getFather().getFather().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos toy " + p2.name);
-                }
-                if (p1.getFather().getMother().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos ths " + p2.name);
+            //Mother
+            if (p1.getMother() != null) {
+                if (p1.getMother().name.equals(p2.name)) {
+                    if (p1.sex.equals("male")) {
+                        System.out.println(p1.name + " is son of " + p2.name);
+                    } else {
+                        System.out.println(p1.name + " is daughter of " + p2.name);
+                    }
                 }
             }
-        }
-        if (p1.getMother() != null) {
-            if (p1.getMother().getMother() != null) {
-                if (p1.getMother().getMother().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos ths " + p2.name);
-                }
-                if (p1.getMother().getFather().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos tou " + p2.name);
-                }
-            } else if (p1.getMother().getFather() != null) {
-                if (p1.getMother().getMother().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos ths " + p2.name);
-                }
-                if (p1.getMother().getFather().name.equals(p2.name)) {
-                    System.out.println(p1.name + " eggonos tou " + p2.name);
-                }
-            }
-        }
 
-        //Grandpas
-        if (p1.children.size() > 0) {
+            //Children
             for (Person child : p1.children) {
-                if (child.children.size() > 0) {
-                    for (Person person : child.children) {
-                        if (person.name.equals(p2.name)) {
-                            if (p1.sex.equals("male")) {
-                                System.out.println(p1.name + " grandpa of " + p2.name);
-                            } else {
-                                System.out.println(p1.name + " grandmother of " + p2.name);
+                if (child.name.equals(p2.name)) {
+                    if (p1.sex.equals("male")) {
+                        System.out.println(p1.name + " is father of " + p2.name);
+                    } else {
+                        System.out.println(p1.name + " is mother of " + p2.name);
+                    }
+                }
+            }
+
+            //Married
+            for (Person person : p1.married) {
+                if (person.name.equals(p2.name)) {
+                    System.out.println(p1.name + " is married with " + p2.name);
+                }
+            }
+
+            ///Siblings
+            for (Person sibling : p1.siblings) {
+                if (sibling.name.equals(p2.name)) {
+                    if (p1.sex.equals("male")) {
+                        System.out.println(p1.name + " is brother of " + p2.name);
+                    } else {
+                        System.out.println(p1.name + " is sister of " + p2.name);
+                    }
+                }
+            }
+
+            //Cousins
+            if (p1.getFather() != null) {
+                for (Person sibling : p1.getFather().siblings) {
+                    for (Person child : sibling.children) {
+                        if (child.name.equals(p2.name)) {
+                            System.out.println(p1.name + " is cousin with " + p2.name + " from " + p1.getFather().name + " and " + sibling.name);
+                        }
+                    }
+                }
+            }
+
+            if (p1.getMother() != null) {
+                for (Person sibling : p1.getMother().siblings) {
+                    for (Person child : sibling.children) {
+                        if (child.name.equals(p2.name)) {
+                            System.out.println(p1.name + " is cousin with " + p2.name + " from " + p1.getMother().name + " and " + sibling.name);
+                        }
+                    }
+                }
+            }
+
+
+            //Eggonia
+            if (p1.getFather() != null) {
+                if (p1.getFather().getFather() != null) {
+                    if (p1.getFather().getFather().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos toy " + p2.name);
+                    }
+                    if (p1.getFather().getMother().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos ths " + p2.name);
+                    }
+                } else if (p1.getFather().getMother() != null) {
+                    if (p1.getFather().getFather().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos toy " + p2.name);
+                    }
+                    if (p1.getFather().getMother().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos ths " + p2.name);
+                    }
+                }
+            }
+            if (p1.getMother() != null) {
+                if (p1.getMother().getMother() != null) {
+                    if (p1.getMother().getMother().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos ths " + p2.name);
+                    }
+                    if (p1.getMother().getFather().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos tou " + p2.name);
+                    }
+                } else if (p1.getMother().getFather() != null) {
+                    if (p1.getMother().getMother().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos ths " + p2.name);
+                    }
+                    if (p1.getMother().getFather().name.equals(p2.name)) {
+                        System.out.println(p1.name + " eggonos tou " + p2.name);
+                    }
+                }
+            }
+
+            //Grandpas
+            if (p1.children.size() > 0) {
+                for (Person child : p1.children) {
+                    if (child.children.size() > 0) {
+                        for (Person person : child.children) {
+                            if (person.name.equals(p2.name)) {
+                                if (p1.sex.equals("male")) {
+                                    System.out.println(p1.name + " grandpa of " + p2.name);
+                                } else {
+                                    System.out.println(p1.name + " grandmother of " + p2.name);
+                                }
                             }
                         }
                     }
                 }
             }
-        }
 
-        //Uncle
-        if (p1.siblings.size() > 0) {
-            for (Person sibling : p1.siblings) {
-                if (sibling.children.size() > 0) {
-                    for (Person child : sibling.children) {
-                        if (child.name.equals(p2.name)) {
-                            System.out.println(p1.name + " Unlce of " + p2.name);
+            //Uncle
+            if (p1.siblings.size() > 0) {
+                for (Person sibling : p1.siblings) {
+                    if (sibling.children.size() > 0) {
+                        for (Person child : sibling.children) {
+                            if (child.name.equals(p2.name)) {
+                                System.out.println(p1.name + " Unlce of " + p2.name);
+                            }
                         }
                     }
                 }
             }
-        }
 
-        //Anipsios
-        if (p1.getFather() != null) {
-            if (p1.getFather().siblings.size() > 0) {
-                for (Person sibling : p1.getFather().siblings) {
-                    if (sibling.name.equals(p2.name)) {
-                        System.out.println(p1.name + " anipsi of " + p2.name);
+            //Anipsios
+            if (p1.getFather() != null) {
+                if (p1.getFather().siblings.size() > 0) {
+                    for (Person sibling : p1.getFather().siblings) {
+                        if (sibling.name.equals(p2.name)) {
+                            System.out.println(p1.name + " anipsi of " + p2.name);
+                        }
                     }
                 }
             }
-        }
 
-        if (p1.getMother() != null) {
-            if (p1.getMother().siblings.size() > 0) {
-                for (Person sibling : p1.getMother().siblings) {
-                    if (sibling.name.equals(p2.name)) {
-                        System.out.println(p1.name + " anipsi of " + p2.name);
+            if (p1.getMother() != null) {
+                if (p1.getMother().siblings.size() > 0) {
+                    for (Person sibling : p1.getMother().siblings) {
+                        if (sibling.name.equals(p2.name)) {
+                            System.out.println(p1.name + " anipsi of " + p2.name);
+                        }
                     }
                 }
+            }
+        } else {
+            if (p1 == null) {
+                System.out.println("Cannot find person  " + name1);
+            }
+            if (p2 == null) {
+                System.out.println("Cannot find person " + name2);
             }
         }
 
---
+++ src/com/slg/MainMenu.java
@@ -0,0 +1,33 @@
+package com.slg;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainMenu {
+
+    String value1,value2;
+
+    public MainMenu() {
+        Main.allPersons = new ArrayList<>();
+        Main.readFamilyTree();
+    }
+
+    public void showMenu() {
+        System.out.println("-----------------------------");
+        System.out.println("Please give 2 names to find the relation between them");
+
+        Scanner scan = new Scanner(System.in);
+        value1 = scan.nextLine();
+        System.out.println("Good now give me another one!");
+        value2 = scan.nextLine();
+
+        findRelation(value1,value2);
+    }
+
+
+    private void findRelation(String value1,String value2) {
+        Main.relationFinder(value1,value2);
+    }
+
+
+}
